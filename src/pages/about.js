a1e7a1354b6de448a20bcde185123a35f34ab805
Zac <zacaree@gmail.com>
2019-01-04 09:03:28 -0600

    first commit

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Layout from '../components/layout';
+
+const about = () => (
+  <Layout>
+    <h1>About</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde quam maiores
+      quas soluta voluptatibus, qui, delectus accusantium sunt quaerat similique
+      voluptate minus illum ipsum, tempora animi odio? Numquam, labore cumque.
+    </p>
+  </Layout>
+);
+
+export default about;
---
+++ src/pages/company/team.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Layout from '../../components/layout';
+
+const team = () => (
+  <Layout>
+    <h1>Team</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde quam maiores
+      quas soluta voluptatibus, qui, delectus accusantium sunt quaerat similique
+      voluptate minus illum ipsum, tempora animi odio? Numquam, labore cumque.
+    </p>
+  </Layout>
+);
+
+export default team;
