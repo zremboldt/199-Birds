5e4ac91cc06ef1eee201796de65f411635e0710c
zacaree <zacharem@gmail.com>
2019-01-04 20:44:43 -0600

    Getting ready to setup dynamic page generation from GraphQL query.

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,36 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+// Implement Gatsby's Node APIs in this file.
+// See: https://www.gatsbyjs.org/docs/node-apis/
 
-// You can delete this file if you're not using it
+// path is part of Node so we don't need to install it
+const path = require('path');
+
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions;
+
+  return new Promise((resolve, reject) => {
+    graphql(`
+      {
+        allMarkdownRemark {
+          edges {
+            node {
+              frontmatter {
+                path
+              }
+            }
+          }
+        }
+      }
+    `).then(results => {
+      results.data.allMarkdownRemark.edges.forEach(({ node }) => {
+        createPage({
+          path: `/posts${node.frontmatter.path}`,
+          component: path.resolve('./src/components/postLayout.js'),
+          context: {
+            slug: node.frontmatter.path,
+          },
+        });
+      });
+      resolve();
+    });
+  });
+};
---
+++ src/components/postLayout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from './layout';
+
+const postLayout = () => {
+  return (
+    <Layout>
+      <h1>postLayout</h1>
+    </Layout>
+  );
+};
+
+export default postLayout;
