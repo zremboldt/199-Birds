c0617f4e1fb341db2c7a7f51905165cd38b2594e
zacaree <zacharem@gmail.com>
2019-01-12 22:08:50 -0600

    Jumping back into this after being sidetracked. Ready for vid 15.

--- src/components/postLayout.js
+++ src/components/postLayout.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 import Layout from './layout';
+
+// Static Query
+// Can be used anywhere but does not accept a variable.
+
+// Page Query
+// Must be used on pages, does accept variables.
 
 const postLayout = () => {
   return (
@@ -10,3 +17,21 @@ const postLayout = () => {
 };
 
 export default postLayout;
+
+// There's a bit of Gatsby magic going on here.
+// There's no explicit hookup between the component above and the query below but
+// the data will come into the component above none-the-less.
+// For this to work though you have to use a named export and call it query here below.
+
+export const query = graphql`
+  query PostQuery {
+    markdownRemark(frontmatter: { path: { eq: "/third-post" } }) {
+      html
+      frontmatter {
+        title
+        date
+        path
+      }
+    }
+  }
+`;
