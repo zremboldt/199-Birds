80266180b7ffba3f75212efa55450c9ba29698f1
zacaree <zacharem@gmail.com>
2019-07-27 16:55:34 -0500

    Starting the about page

---
+++ src/pages/about.js
@@ -0,0 +1,66 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="pageAbout">
+      <h3>About this app</h3>
+      <p>
+        This web app is a personal project made for my family/kiddos to enjoy.
+        Our three year old loves the Usborne 199 Birds book and the other day we
+        were looking up the birds on YouTube to see them in action and hear what
+        they sound like. I thought it'd be fun to make an easier way to find the
+        videos without worrying about ads or reccomended videos popping up.
+        Since it's just for us I didn't have to worry about browser
+        compatability issues.
+      </p>
+
+      <h3>Fun technical stuff</h3>
+      <ul>
+        <li>Site is built using React / Gatsby.js. Styling is done in Sass.</li>
+        <li>Gatsby Image offers a number of amazing benefits:</li>
+        <ul>
+          <li>
+            Webp images are served rather than jpgs if the browser supports
+            them. This drastically improves data consumption and page load
+            times.
+          </li>
+          <li>
+            Initial page load is super quick because low res images are loaded
+            as placeholders until full res versions load and fade in to take
+            their place. This also keeps your layout from jumping around while
+            the images load.
+          </li>
+          <li>
+            Images below the fold are lazy loaded using an Intersection
+            Observer. So they never load unless the user scrolls down to that
+            position on the page. This speeds up initial page load and saves
+            bandwidth.
+          </li>
+        </ul>
+        <li>
+          Search bar uses CSS sticky positioning to snap it to the top of the
+          viewport.
+        </li>
+        <li>
+          Search results dynamically update as you enter each character so
+          finding the flamingo is as easy as typing "fl".
+        </li>
+        <li>
+          Adding a bird is as easy as adding an object to birdData.js. Just give
+          it a name and the YouTube video ID. You can also assign an optional
+          video start and end time.
+        </li>
+        <li>
+          App is designed to work great on all device sizes from desktop to
+          mobile.
+        </li>
+        <li>
+          Lighthouse score is all green. Still have a couple of tweaks I can
+          make to further improve performance/accessibility.
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default About;
